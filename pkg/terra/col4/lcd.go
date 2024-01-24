4cd843721ee970c737511ab7c4aabe20efd2a232
joon <joon@delightlabs.io>
2024-01-24 18:12:49 +0900

    feat: lcd, rpc, fcd client

---
+++ pkg/terra/col4/lcd.go
@@ -0,0 +1,83 @@
+package col4
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/pkg/errors"
+)
+
+type Lcd interface {
+	ContractState(address string, query string, height ...uint64) ([]byte, error)
+	Tx(hash string) (*LcdTxRes, error)
+}
+
+type lcdImpl struct {
+	baseUrl string
+	client  *http.Client
+}
+
+func NewLcd(baseUrl string, client *http.Client) Lcd {
+	return &lcdImpl{baseUrl, client}
+}
+
+// Tx implements Lcd.
+func (l *lcdImpl) Tx(hash string) (*LcdTxRes, error) {
+	reqUrl := fmt.Sprintf("%s/txs/%s", l.baseUrl, hash)
+	response, err := l.client.Get(reqUrl)
+	if err != nil {
+		return nil, errors.Wrap(err, "lcdImpl.ContractStateRes")
+	}
+	defer response.Body.Close()
+
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "lcdImpl.ContractStateRes")
+	}
+	LcdTxRes := LcdTxRes{}
+
+	if err := json.Unmarshal(data, &LcdTxRes); err != nil {
+		return nil, errors.Wrap(err, "lcdImpl.ContractStateRes")
+	}
+
+	return &LcdTxRes, nil
+}
+
+func (l *lcdImpl) ContractState(address string, query string, height ...uint64) ([]byte, error) {
+	params := url.Values{}
+	params.Add("query_msg", query)
+	if len(height) > 0 {
+		params.Add("height", fmt.Sprintf("%d", height[0]))
+	}
+
+	reqUrl := fmt.Sprintf("%s/wasm/contracts/%s/store?%s", l.baseUrl, address, params.Encode())
+	response, err := l.client.Get(reqUrl)
+	if err != nil {
+		return nil, errors.Wrap(err, "lcdImpl.ContractStateRes")
+	}
+	defer response.Body.Close()
+
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "lcdImpl.ContractStateRes")
+	}
+
+	return data, nil
+}
+
+func QueryContractState[T any](lcd Lcd, address string, query string, height ...uint64) (*LcdContractStateRes[T], error) {
+	resBytes, err := lcd.ContractState(address, query, height...)
+	if err != nil {
+		return nil, err
+	}
+
+	var result LcdContractStateRes[T]
+	if err := json.Unmarshal(resBytes, &result); err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
---
+++ pkg/terra/col4/rpc.go
@@ -0,0 +1,97 @@
+package col4
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/pkg/errors"
+)
+
+const (
+	rpcBlockPath        = "block"
+	rpcBlockResultsPath = "block_results"
+)
+
+type Rpc interface {
+	RemoteBlockHeight() (uint64, error)
+	Block(height ...uint64) (*RpcRes[RpcBlockRes], error)
+	BlockResults(height ...uint64) (*RpcRes[RpcBlockResultRes], error)
+}
+
+type rpcImpl struct {
+	baseUrl string
+	client  *http.Client
+}
+
+func NewRpc(baseUrl string, client *http.Client) Rpc {
+	return &rpcImpl{baseUrl, client}
+}
+
+// Block implements Rpc.
+func (r *rpcImpl) Block(height ...uint64) (*RpcRes[RpcBlockRes], error) {
+	url := fmt.Sprintf("%s/%s", r.baseUrl, rpcBlockPath)
+	if len(height) > 0 {
+		url = fmt.Sprintf("%s?height=%d", url, height[0])
+	}
+	response, err := r.client.Get(url)
+	if err != nil {
+		return nil, errors.Wrap(err, "rpcImpl.Block")
+	}
+	defer response.Body.Close()
+
+	data, _ := io.ReadAll(response.Body)
+
+	var res RpcRes[RpcBlockRes]
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, errors.Wrap(err, "rpcImpl.Block")
+	}
+
+	return &res, nil
+}
+
+// BlockResults implements Rpc.
+func (r *rpcImpl) BlockResults(height ...uint64) (*RpcRes[RpcBlockResultRes], error) {
+	url := fmt.Sprintf("%s/%s", r.baseUrl, rpcBlockResultsPath)
+	if len(height) > 0 {
+		url = fmt.Sprintf("%s?height=%d", url, height[0])
+	}
+	response, err := r.client.Get(url)
+	if err != nil {
+		return nil, errors.Wrap(err, "rpcImpl.BlockResults")
+	}
+	defer response.Body.Close()
+
+	data, _ := io.ReadAll(response.Body)
+
+	var res RpcRes[RpcBlockResultRes]
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, errors.Wrap(err, "rpcImpl.RemoteBlockHeight")
+	}
+
+	return &res, nil
+}
+
+// RemoteBlockHeight implements Rpc.
+func (r *rpcImpl) RemoteBlockHeight() (uint64, error) {
+	response, err := r.client.Get(fmt.Sprintf("%s/%s", r.baseUrl, rpcBlockResultsPath))
+	if err != nil {
+		return 0, errors.Wrap(err, "rpcImpl.RemoteBlockHeight")
+	}
+	defer response.Body.Close()
+
+	data, _ := io.ReadAll(response.Body)
+
+	var res RpcRes[RpcBlockResultRes]
+	if err := json.Unmarshal(data, &res); err != nil {
+		return 0, errors.Wrap(err, "rpcImpl.RemoteBlockHeight")
+	}
+
+	height, err := strconv.ParseInt(res.Result.Height, 10, 64)
+	if err != nil {
+		return 0, errors.Wrap(err, "rpcImpl.RemoteBlockHeight")
+	}
+	return uint64(height), nil
+}
