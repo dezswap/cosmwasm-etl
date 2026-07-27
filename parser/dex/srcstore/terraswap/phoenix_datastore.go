0245358cfaa690fb0db7813f1143e163d8e0950f
Jihyung Lee <young@delightlabs.io>
2026-07-27 13:45:57 +0900

    feat(parser): support Cosmos SDK v0.50 upgrade on columbus-5

--- parser/dex/srcstore/terraswap/phoenix_datastore.go
+++ parser/dex/srcstore/terraswap/phoenix_datastore.go
@@ -2,15 +2,12 @@ package terraswap
 
 import (
 	"encoding/json"
-	"time"
 
 	"github.com/dezswap/cosmwasm-etl/parser"
-	"github.com/dezswap/cosmwasm-etl/pkg/eventlog"
-	"github.com/dezswap/cosmwasm-etl/pkg/terra/lcd"
-
 	"github.com/dezswap/cosmwasm-etl/parser/dex"
 	"github.com/dezswap/cosmwasm-etl/pkg/dex/terraswap"
 	"github.com/dezswap/cosmwasm-etl/pkg/terra/cosmos45"
+	"github.com/dezswap/cosmwasm-etl/pkg/terra/lcd"
 	"github.com/dezswap/cosmwasm-etl/pkg/terra/rpc"
 	"github.com/pkg/errors"
 )
@@ -26,61 +23,21 @@ type cosmos47Resultog struct {
 }
 
 type phoenixSourceDataStore struct {
-	rpc rpc.Rpc
-	terraswap.QueryClient
-	chainDataAdapter
+	*baseRawDataStoreImpl
 }
 
 var _ dex.SourceDataStore = &phoenixSourceDataStore{}
 
 func NewPhoenixStore(factoryAddress string, rpc rpc.Rpc, lcd lcd.Lcd[cosmos45.LcdTxRes], client terraswap.QueryClient) dex.SourceDataStore {
 	return &phoenixSourceDataStore{
-		rpc,
-		client,
-		&col5ChainDataAdapter{
+		baseRawDataStoreImpl: &baseRawDataStoreImpl{rpc, client, &col5ChainDataAdapter{
 			factoryAddress: factoryAddress,
 			mapper:         &mapperImpl{},
 			rpc:            rpc,
 			lcd:            lcd,
 			QueryClient:    client,
-		},
+		}},
 	}
-}
-
-func (r *phoenixSourceDataStore) GetPoolInfos(height uint64) ([]dex.PoolInfo, error) {
-	allPairs, err := r.AllPairs(height)
-	if err != nil {
-		return nil, errors.Wrap(err, "phoenixSourceDataStore.GetPoolInfos")
-	}
-
-	poolInfos := make([]dex.PoolInfo, len(allPairs))
-
-	for idx, pair := range allPairs {
-		poolRes, err := r.QueryPool(pair.ContractAddr, height)
-		if err != nil {
-			return nil, errors.Wrap(err, "phoenixSourceDataStore.GetPoolInfos")
-		}
-
-		poolInfos[idx] = dex.PoolInfo{
-			ContractAddr: pair.ContractAddr,
-			Assets: []dex.Asset{
-				{Addr: pair.Assets[0], Amount: poolRes.Assets[0].Amount},
-				{Addr: pair.Assets[1], Amount: poolRes.Assets[1].Amount},
-			},
-			LpAddr:     pair.LpAddr,
-			TotalShare: poolRes.TotalShare,
-		}
-	}
-	return poolInfos, nil
-}
-
-func (r *phoenixSourceDataStore) GetSourceSyncedHeight() (uint64, error) {
-	height, err := r.rpc.RemoteBlockHeight()
-	if err != nil {
-		return 0, errors.Wrap(err, "phoenixSourceDataStore.GetSourceSyncedHeight")
-	}
-
-	return height, nil
 }
 
 func (r *phoenixSourceDataStore) GetSourceTxs(height uint64) (parser.RawTxs, error) {
@@ -111,7 +68,7 @@ func (r *phoenixSourceDataStore) GetSourceTxs(height uint64) (parser.RawTxs, err
 		var tx parser.RawTx
 		var err error
 		if height > cosmosSdk50StartHeight {
-			tx, err = r.convertLogToRawTx(txHash, txResults[i].Events, blockTime)
+			tx, err = r.convertEventsToRawTx(txHash, txResults[i].Events, blockTime)
 		} else {
 			var logs []cosmos47Resultog
 			if err = json.Unmarshal([]byte(txResults[i].Log), &logs); err != nil {
@@ -121,7 +78,7 @@ func (r *phoenixSourceDataStore) GetSourceTxs(height uint64) (parser.RawTxs, err
 			for _, l := range logs {
 				events = append(events, l.Events...)
 			}
-			tx, err = r.convertLogToRawTx(txHash, events, blockTime)
+			tx, err = r.convertEventsToRawTx(txHash, events, blockTime)
 		}
 		if err != nil {
 			return nil, err
@@ -130,57 +87,4 @@ func (r *phoenixSourceDataStore) GetSourceTxs(height uint64) (parser.RawTxs, err
 		rawTxs = append(rawTxs, tx)
 	}
 	return rawTxs, nil
-}
-
-func (r *phoenixSourceDataStore) convertLogToRawTx(txHash string, events []rpc.RpcEventRes, blockTs time.Time) (parser.RawTx, error) {
-	logResultMap := r.groupLogAttrByType(events)
-	tx := parser.RawTx{
-		Hash:       txHash,
-		Timestamp:  blockTs,
-		LogResults: make([]eventlog.LogResult, 0, len(logResultMap)),
-	}
-
-	for logType, logs := range logResultMap {
-		tx.LogResults = append(tx.LogResults, eventlog.LogResult{
-			Type:       logType,
-			Attributes: logs,
-		})
-		if logType == eventlog.Message {
-			for _, attr := range logs {
-				if attr.Key == "sender" {
-					tx.Sender = attr.Value
-					break
-				}
-			}
-		}
-	}
-
-	var err error
-	if tx.Sender == "" {
-		if tx.Sender, err = r.TxSenderOf(txHash); err != nil {
-			return parser.RawTx{}, errors.Wrapf(err, "failed to retrieve sender for tx %s from TxSenderOf", txHash)
-		}
-	}
-
-	return tx, nil
-}
-
-func (r *phoenixSourceDataStore) groupLogAttrByType(events []rpc.RpcEventRes) map[eventlog.LogType]eventlog.Attributes {
-	logResultMap := make(map[eventlog.LogType]eventlog.Attributes)
-
-	for _, event := range events {
-		attributes := eventlog.Attributes{}
-		for _, attr := range event.Attributes {
-			attributes = append(attributes, eventlog.Attribute{
-				Key:   attr.Key,
-				Value: attr.Value,
-			})
-		}
-		logType := eventlog.LogType(event.Type)
-		if attrs, ok := logResultMap[logType]; ok {
-			attributes = append(attrs, attributes...)
-		}
-		logResultMap[logType] = attributes
-	}
-	return logResultMap
 }
